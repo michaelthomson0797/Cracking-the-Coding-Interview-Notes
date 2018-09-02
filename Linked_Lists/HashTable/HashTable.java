85cbf8edf13b19765d2013482a0c899b8305fd9d
Michael Thomson <michaelthomson0797@gmail.com>
2018-09-02 16:24:54 +0000

    completed hash table implementation with chaining

---
+++ Linked_Lists/HashTable/HashTable.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class HashTable<K,V> {
+    private ArrayList<LinkedList<K,V>> table;
+    private int size;
+
+    public HashTable(int size) {
+        this.table = new ArrayList<LinkedList<K,V>>(size);
+        for(int i = 0; i < size; i++) {
+            table.add(new LinkedList<K,V>());
+        }
+        this.size = size;
+    }
+
+    private int hash(K key) {
+        return key.hashCode() % size;
+    }
+
+    public void insert(K key, V value) {
+        int index = hash(key);
+
+        table.get(index).appendToStart(key, value);
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+
+        return table.get(index).find(key).value;
+    }
+
+    public void delete(K key) {
+        int index = hash(key);
+
+        table.get(index).delete(key);
+    }
+
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+
+        for(int i = 0; i < size; i++) {
+            string.append(table.get(i).toString() + "\n");
+        }
+
+        return string.toString();
+    }
+
+    public static void main(String[] args) {
+        HashTable<Integer, String> table = new HashTable<Integer, String>(31);
+
+        table.insert(0, "hello");
+        table.insert(2, "goodbye");
+        table.insert(5, "test");
+        table.insert(31, "foo");
+
+        table.delete(5);
+
+        System.out.print(table.toString());
+    }
+
+}
+
---
+++ Linked_Lists/HashTable/LinkedList.java
@@ -0,0 +1,123 @@
+/**
+ * A singly linked list which allows duplicates
+ *
+ * @author Michael Thomson
+ */
+
+public class LinkedList<K,V> {
+    Node head;
+
+    /**
+     * A member of a singly linked list containing a key, value pair
+     */
+    class Node {
+        K key;
+        V value;
+        Node next;
+        
+        /**
+         * constructor for a node
+         *
+         * @param key key value
+         * @param value value to be mapped to the key
+         * @param next reference to next node in the list
+         */
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    /**
+     * constructor for a linked list
+     */
+    public LinkedList() {
+        head = null;
+    }
+
+    /**
+     * finds the node in the linked list with the given key and returns it
+     *
+     * @param key key to search for
+     * @return the node asociated with the given key
+     */
+    public Node find(K key) {
+        Node x;
+        for(x = head; x.key != key && x != null; x = x.next);
+        return x;
+    }
+
+    /**
+     * appends the given key, value pair to the start of the linked list
+     *
+     * @param key key value to be inserted
+     * @param value value to be inserted with the key
+     */
+    public void appendToStart(K key, V value) {
+        if(head != null) {
+            Node newNode = new Node(key, value, head);
+            this.head = newNode;
+        } else {
+            Node newNode = new Node(key, value, null);
+            this.head = newNode;
+        }
+    }
+
+    /**
+     * appends the given key, value pair to the end of the linked list
+     *
+     * @param key key value to be inserted
+     * @param value value to be inserted with the key
+     */
+    public void appendToEnd(K key, V value) {
+        Node x;
+        if(head != null) {
+            for(x = head; x.next != null; x = x.next);
+            x.next = new Node(key, value, null);
+        } else {
+            head = new Node(key, value, null);
+        }
+    }
+    
+    /**
+     * deletes the node with the given key from the linked list
+     *
+     * @param key key value to be deleted from the list
+     */
+    public void delete(K key) {
+        if(head == null) {
+            return;
+        } else if(head.key == key) {
+            head = head.next;
+            return;
+        }
+        
+        Node x;
+        for(x = head; x.next != null && x.next.key != key; x = x.next);
+
+        x.next = x.next.next;
+    }
+
+
+    /**
+     * returns the linked list in a string representation of the form:
+     * {(k1,v1),(k2,v2),...,(kn,vn)}
+     */
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("{");
+
+        for(Node x = head;x != null; x = x.next) {
+            stringBuilder.append("(" + x.key.toString() + "," + x.value.toString() + ")");
+            if(x.next != null) {
+                stringBuilder.append(",");
+            }
+        }
+
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
+    }
+}
