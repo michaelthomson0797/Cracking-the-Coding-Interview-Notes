050160c5e8320e96ea3272d3b43cc8eb499421a6
Michael Thomson <michaelthomson0797@gmail.com>
2018-09-03 11:10:05 +0000

    completed linked list implementation of a Queue

---
+++ Implementations/Queue/Queue_LL.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Queue_LL<T> {
+    private static class QueueNode<T> {
+        private T data;
+        private QueueNode<T> next;
+
+        public QueueNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+    public void add(T item) {
+        QueueNode<T> t = new QueueNode<T>(item);
+        if(last != null) {
+            last.next = t;
+        }
+        last = t;
+        if (first == null) {
+            first = last;
+        }
+    }
+
+    public T remove() {
+        if (first == null) throw new NoSuchElementException();
+        T data = first.data;
+        first = first.next;
+        if(first == null) {
+            last = null;
+        }
+        return data;
+    }
+
+    public T peek() {
+        if (first == null) throw new NoSuchElementException();
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public static void main(String[] args) {
+        Queue_LL<Integer> queue = new Queue_LL<Integer>();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+    }
+}
